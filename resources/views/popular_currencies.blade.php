b819bd263df64ecf5013d86fe8c0b1861a6b57bf
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-07 01:31:57 +0300

    Work#2. Add CurrencyPresenter. Add api/currencies in routes/api.php. Test OK.

---
+++ resources/views/popular_currencies.blade.php
@@ -0,0 +1,6 @@
+<ul>
+
+@foreach($popArray as $p)
+    <li>{{$p}}</li>
+    @endforeach
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'currencies'] , function (){
+
+    Route::get('/popular' , 'PopularController@index');
+
+});
