90ad2bfd14c3b4c6df9e2d593041fa5b6837ac14
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-07 01:54:12 +0300

    Work#2. Test OK.

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Services\CurrencyPresenter;
 use App\Services\CurrencyRepositoryInterface;
+use App\Services\GetMostChangedCurrencyCommandHandler;
 use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
     public $repository;
+    //public $json_unstable;
 
     public function __construct(CurrencyRepositoryInterface $repository) {
         $this->repository = $repository;
@@ -21,5 +23,15 @@ class CurrencyController extends Controller
             $json_currency[] = CurrencyPresenter::present($currency);
         }
         return response()->json($json_currency);
+    }
+
+    public function unstable()
+    {
+        $unstable = app(GetMostChangedCurrencyCommandHandler::class);
+//        dd($unstable);
+        $uns_arr = CurrencyPresenter::present($unstable->handle());
+//        dd($uns_arr);
+        return response()->json($uns_arr);
+
     }
 }
