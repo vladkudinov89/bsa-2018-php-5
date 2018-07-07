d72e8af13a3f96035edb6de3389b3477cd5c460c
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-07 03:51:43 +0300

    Add view popular_currencies

--- resources/views/popular_currencies.blade.php
+++ resources/views/popular_currencies.blade.php
@@ -1,6 +1,22 @@
-<ul>
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Icon</th>
+        <th>Price</th>
+        <th>DailyChngPer</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($popArray as $p)
+        <tr>
+            <td>{{$p->getName()}}</td>
+            <td><img src="{{$p->getImageUrl()}}" alt=""></td>
+            <td>{{$p->getPrice()}} $</td>
+            <td>{{$p->getDailyChangePercent()}} %</td>
+        </tr>
+        @endforeach
 
-@foreach($popArray as $p)
-    <li>{{$p}}</li>
-    @endforeach
-</ul>
+    </tbody>
+</table>
+
